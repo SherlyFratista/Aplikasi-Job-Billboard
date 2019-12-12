f4ab53e9429f81726c8b3bf6079fb7baf560121b
SherlyFratista <48596801+SherlyFratista@users.noreply.github.com>
2019-12-12 12:59:16 +0700

    update edit profile

--- edit_profile.php
+++ edit_profile.php
@@ -101,7 +101,7 @@ $email = $_SESSION['email'];
 				<li class="nav-item dropdown">
 				<a class="btn btn-primary dropdown-toggle" href="" id="navbardrop" data-toggle="dropdown"><?= $user?></a>
 					<div class="dropdown-menu">
-						<a class="dropdown-item" href="controller/redirect_profile.php">Profile</a>
+					    <a class="dropdown-item" href="homepage.php">Home</a>
 						<a class="dropdown-item" href="controller/logout.php">Logout</a>
 					</div>
 				</li>
